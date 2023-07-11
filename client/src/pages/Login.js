f0ae4f51bcb7072dcf558425d5bf7a38b2b9053e
[Kayla Norris] <[kayla.e.norris@gmail.com]>
2023-07-10 21:03:35 -0400

    added styling to signup and login files

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -31,39 +31,41 @@ function Login(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+      <Link to="/signup" style={{ color: '#00adef' }}>← Go to Signup</Link>
 
       <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">We're sorry, the provided credentials are incorrect.Please try again.</p>
+      <div className="center-content">
+        <form onSubmit={handleFormSubmit}>
+          <div className="flex-row space-between my-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Login</button>
-        </div>
-      </form>
+          <div className="flex-row space-between my-2">
+            <label htmlFor="pwd">Password:</label>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+          {error ? (
+            <div>
+              <p className="error-text">We're sorry, the provided credentials are incorrect.Please try again.</p>
+            </div>
+          ) : null}
+          <div className="button">
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
